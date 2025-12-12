c1053423cc7a98a30b664666a7837db31c146071
Paul Hart <github@paulhart.ca>
2025-12-11 22:19:27 -0500

    more differentiation

--- custom_components/mopidy/const.py
+++ custom_components/mopidy/const.py
@@ -3,7 +3,7 @@ from collections import OrderedDict
 
 DOMAIN = "mopidy"
 ICON = "mdi:speaker-wireless"
-DEFAULT_NAME = "Mopidy"
+DEFAULT_NAME = "Mopidy Enhanced"
 DEFAULT_PORT = 6680
 SERVICE_SET_CONSUME_MODE = "set_consume_mode"
 SERVICE_SNAPSHOT = "snapshot"
